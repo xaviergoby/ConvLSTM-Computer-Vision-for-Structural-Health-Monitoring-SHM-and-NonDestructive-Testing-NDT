cd2ff8408c9ee1b6f40496b2c81561bacdac4844
vewald <30593946+vewald@users.noreply.github.com>
2020-05-27 17:13:20 +0200

    Results update
    Results update Run3 27-MAY-2020

--- call_mainscript.py
+++ call_mainscript.py
@@ -13,24 +13,24 @@ Created on Sat May 16 13:45:50 2020
 from main_v2 import mainscript
 import numpy as np
 
-bs = 32
-
-frame_width = np.array([128, 256, 512, 1024])
-frame_height = 247
-channels = 3
-img_colour_format = "rgb"
-data_dir_name = ["scene1_rgb_sensor123", "scene2_rgb_sensor123", "scene3_rgb_sensor123", "scene4_rgb_noise"]
-num_epochs = 500
-
-for ddr in data_dir_name:
-    for fw in frame_width:
-        mainscript.training(ddr, fw, frame_height, channels, img_colour_format, bs, num_epochs)
+bs = 16
 
 frame_width = np.array([128, 257, 512, 1024])
 frame_height = 193
 channels = 1
 img_colour_format = "gray_scale"
 data_dir_name = ["scene1_grey_sensor1", "scene2_grey_sensor1", "scene3_grey_sensor1", "scene4_grey_noise"]
+num_epochs = 500
+
+for ddr in data_dir_name:
+    for fw in frame_width:
+        mainscript.training(ddr, fw, frame_height, channels, img_colour_format, bs, num_epochs)
+        
+frame_width = np.array([128, 256, 512, 1024])
+frame_height = 247
+channels = 3
+img_colour_format = "rgb"
+data_dir_name = ["scene1_rgb_sensor123", "scene2_rgb_sensor123", "scene3_rgb_sensor123", "scene4_rgb_noise"]
 num_epochs = 500
 
 for ddr in data_dir_name:
