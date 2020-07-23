c7125028302bf7ce3cef6ef4090e961b79a6dbc0
vewald <30593946+vewald@users.noreply.github.com>
2020-07-23 16:31:54 +0200

    Revert "Update Results"
    This reverts commit 05cd5683acb430864c669a5ee5d52ea72bb2ff80.

--- src/lstm_models/lstm_models_collection.py
+++ src/lstm_models/lstm_models_collection.py
@@ -1,20 +1,20 @@
 from keras.layers import GRU, LSTM, Dense, Dropout, LeakyReLU
 
-do_rate = 0.50
+do_rate = 0.25
 
 def get_simple_single_layer_fc_lstm_func_api_model_output(model_input, num_classes):
     # LSTM: First and following return sequences must be True, Last return sequence must be False
 
     lstm_1 = GRU(16, return_sequences=True, activation="relu")(model_input)
     do1 = Dropout(do_rate)(lstm_1)
-    lstm_2 = GRU(16, return_sequences=False, activation="relu")(do1)
+    lstm_2 = GRU(16, return_sequences=True, activation="relu")(do1)
     do2 = Dropout(do_rate)(lstm_2)
-    #lstm_3 = GRU(16, return_sequences=True, activation="relu")(do2)
-    #do3 = Dropout(do_rate)(lstm_3)
-    #lstm_4 = GRU(16, return_sequences=False, activation="relu")(do3)
-    #do4 = Dropout(do_rate)(lstm_4)
+    lstm_3 = GRU(16, return_sequences=True, activation="relu")(do2)
+    do3 = Dropout(do_rate)(lstm_3)
+    lstm_4 = GRU(16, return_sequences=False, activation="relu")(do3)
+    do4 = Dropout(do_rate)(lstm_4)
     #MLP
     #dense_1 = Dense(16, activation="relu")(do4)
     #do3 = Dropout(do_rate)(dense_1)
-    dense_2 = Dense(num_classes, activation="softmax")(do2)
+    dense_2 = Dense(num_classes, activation="softmax")(do4)
     return dense_2
